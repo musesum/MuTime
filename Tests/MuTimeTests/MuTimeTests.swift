8669204ea2dafadfa6e562ae4d67649ce2cdd939
musesum <musesum@gmail.com>
2023-01-25 09:13:22 +0800

    add .

---
+++ Tests/MuTimeTests/MuTimeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MuTime
+
+final class MuTimeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(MuTime().text, "Hello, World!")
+    }
+}
